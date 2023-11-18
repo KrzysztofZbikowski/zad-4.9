9da8c0ee17f088546f254b726821dd01f88c0c0d
Zbiku <Zbiku@MSI>
2023-11-18 23:47:48 +0100

    Dodaj pliki projektów.

---
+++ ConsoleApp1/Program.cs
@@ -0,0 +1,13 @@
+﻿class zad
+{
+    static void Main(string[] args)
+    {
+        int liczba;
+        Console.WriteLine("Podaj łancuch znaków: ");
+        string znaki = Console.ReadLine();
+        string[] wyrazy = znaki.Split(' ');
+        liczba = wyrazy.Length;
+        Console.WriteLine("Liczba wyrazów w twoim ciągu znaków wynosi: " + liczba);
+        Console.ReadKey();
+    }
+}
